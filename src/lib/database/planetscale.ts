dfafdfeb9566fb96b077292dbcb269b3e4e0a0ea
Tobias <tobiaswutz22@gmail.com>
2023-03-30 21:42:40 +0200

    envs

--- src/lib/database/planetscale.ts
+++ src/lib/database/planetscale.ts
@@ -1,9 +1,11 @@
 import { connect } from '@planetscale/database';
 
+import { VITE_DATABASE_HOST, VITE_DATABASE_USERNAME, VITE_DATABASE_PASSWORD } from '$env/static/private';
+
 const config = {
-  host: import.meta.env.VITE_DATABASE_HOST,
-  username: import.meta.env.VITE_DATABASE_USERNAME,
-  password: import.meta.env.VITE_DATABASE_PASSWORD
+  host: VITE_DATABASE_HOST,
+  username: VITE_DATABASE_USERNAME,
+  password: VITE_DATABASE_PASSWORD
 };
 
 export async function getPlanetScaleConnection() {
